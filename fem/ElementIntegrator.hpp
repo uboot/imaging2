50bf18be5d25198204ffc57365ef237913c36c38
Matthias Fuchs <matz.fuchs@gmx.at>
2012-10-09 20:49:56 +0200

    Add GPL header to core, fem, graphics

--- fem/ElementIntegrator.hpp
+++ fem/ElementIntegrator.hpp
@@ -1,10 +1,21 @@
-// This file is part of the imaging2 class library.
-//
-// University of Innsbruck, Infmath Imaging, 2009.
-// http://infmath.uibk.ac.at
-//
-// All rights reserved.
-
+/* 
+*  Copyright 2009 University of Innsbruck, Infmath Imaging
+*
+*  This file is part of imaging2.
+*
+*  Imaging2 is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  Imaging2 is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with stromx-studio.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef FEM_ELEMENTINTEGRATOR_H
 #define FEM_ELEMENTINTEGRATOR_H
--- fem/equation/GeodesicActiveContourStep.hpp
+++ fem/equation/GeodesicActiveContourStep.hpp
@@ -1,3 +1,22 @@
+/* 
+*  Copyright 2009 University of Innsbruck, Infmath Imaging
+*
+*  This file is part of imaging2.
+*
+*  Imaging2 is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  Imaging2 is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with stromx-studio.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef EQUATION_GEODESICACTIVECONTOURSTEP_H
 #define EQUATION_GEODESICACTIVECONTOURSTEP_H
 
